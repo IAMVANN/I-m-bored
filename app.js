cd7f66ba4e405afd294e3a1929bbd614388b7817
IAMVANN <izheng@ucvts.org>
2020-04-10 17:33:03 -0400

    speedFIx

--- app.js
+++ app.js
@@ -15,7 +15,8 @@
 
 */
 const plainlvl = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] // each one is worth 50 pixels. 15* 50 = 750;
-const lv1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] //triagnle every 150 pixels
+const lv1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] //triagnle every 150 pixels // 150 * 10 = 1500 pixels
+
 const arrayMaster = [ lv1, plainlvl]
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let start;
@@ -62,7 +63,7 @@ function init(object){
         start.remove();
         canvas.clearRect(0, 0, 1500, 500);
         // Make all init variables
-        game = setInterval(action, 15);
+        game = setInterval(action, 5);
 
 
     }
@@ -88,7 +89,7 @@ function action(){
 function unpack(){
     let rando = Math.random();
     if(loading == true){
-        if(gametime % 30 == 0){
+        if(gametime % 10 == 0){
             currentObject = currentArray[arrayposition];
             arrayposition++;
             if(arrayposition == 10){
@@ -139,19 +140,9 @@ function position(){
 
 }
 function refresh(){
-    for(var x = 0; x <= 1500; x++) {
-
-            // Get the pixel at this location
-            if(x <= 1495 ){
-                var pixelfront = canvas.getImageData(x+5, 0, 1, 500);
-
-            } else {
-                canvas.clearRect(x, 0, 1, 500 )
-            }
-            canvas.putImageData(pixelfront, x , 0);
-
-        }
-
+        var pixelfront = canvas.getImageData(5, 0, 1500, 500);
+        canvas.clearRect(1495, 0, 5, 500);
+        canvas.putImageData(pixelfront, 0, 0);
 }
 function render(){
     canvas.beginPath();
@@ -180,13 +171,11 @@ function render(){
         currentObject = undefined;
     }
 }
-function adder(p, p2, l1, l2){
-    hitRay[hitRayPos] ;
+function adder(x1, x2, y1, y2){
+    hitRay[hitRayPos];
 }
 function direction(event){
-    if(event.keyCode == 37){
-        movement = "left";
-    } else if (event.keyCode == 38){
+    if (event.keyCode == 32){
         if(used == false){
             movement = "top"
             used = true;
